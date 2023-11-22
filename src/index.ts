11473674f7beef385122fe3e8f0f392125ccf058
Dom <domeccleston@gmail.com>
2023-11-22 10:47:22 +0000

    start implementing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,53 @@
-const program = require("commander");
+import { Command } from "commander";
+import { customAlphabet } from "nanoid";
 
-program.option("--first")
+const program = new Command();
+const nanoid = customAlphabet("123456789QAZWSXEDCRFVTGBYHNUJMIKOLP", 8);
 
-program.parse()
+program
+  .name("unkey-cli")
+  .description("Example CLI application with Unkey auth")
+  .version("0.0.1");
 
-console.log(program.opts())
+program
+  .command("login")
+  .description("Authenticate with your service via the CLI")
+  .action(() => {
+    const id = nanoid();
+    console.log(`Confirmation code: ${id}`);
+  });
+
+program.parse();
+
+// type AuthConfig = {
+//   token: string;
+// };
+
+// function openLoginPage(): AuthConfig {
+//   return { token: "123" };
+// }
+
+// async function writeCredentialsToFile() {}
+
+// async function getLocalCredentials(): Promise<AuthConfig> {
+//   return new Promise(() => {
+//     token: "123";
+//   });
+// }
+
+// async function login() {
+//   const { token } = await getLocalCredentials();
+
+//   if (token) {
+//     console.log("already logged in!");
+//   } else {
+//     try {
+//       const result = await openLoginPage();
+//       await writeCredentialsToFile();
+//       console.log("logged in!");
+//       return result;
+//     } catch (error) {
+//       throw new Error();
+//     }
+//   }
+// }
