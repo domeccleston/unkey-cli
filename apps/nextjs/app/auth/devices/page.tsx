19cfaa743a8954bfee514a5a38fd2ee5b6b9e6fc
Dom <domeccleston@gmail.com>
2024-01-08 18:38:12 +0000

    cancellation ui

--- apps/nextjs/app/auth/devices/page.tsx
+++ apps/nextjs/app/auth/devices/page.tsx
@@ -3,7 +3,7 @@
 import { useSearchParams, useRouter, notFound } from "next/navigation";
 import { useState } from "react";
 
-import { Fingerprint } from "lucide-react";
+import { Fingerprint, XCircle } from "lucide-react";
 import toast, { Toaster } from "react-hot-toast";
 
 import { Button } from "@/components/ui/button";
@@ -17,19 +17,23 @@ function CodeCharacter({ char }: { char: string }) {
 	);
 }
 
-async function cancel(localServer: string) {
-	await fetch("/api/unkey/cancel", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ localServer }),
-	});
-}
-
 export default function Page() {
 	const [loading, setLoading] = useState(false);
+	const [cancelled, setCancelled] = useState(false);
+
 	const router = useRouter();
+
+	async function cancel(localServer: string) {
+		await fetch("/api/unkey/cancel", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ localServer }),
+		});
+
+		setCancelled(true);
+	}
 
 	async function verify(opts: {
 		code: string | null;
@@ -77,41 +81,53 @@ export default function Page() {
 
 	return (
 		<div className="w-full min-h-screen flex items-center pt-[250px] px-4 flex-col">
-			<div className="flex flex-col">
-				<div className="flex ">
-					<div className="flex justify-center items-center pr-4">
-						<Fingerprint className="text-gray-100" />
+			{cancelled ? (
+				<div className="flex pt-10">
+					<div className="flex justify-center items-center pr-10">
+						<XCircle className="text-gray-100" />
 					</div>
 					<div className="flex-col">
-						<h1 className="text-lg text-gray-100">Device confirmation</h1>
-						<p className="text-sm text-gray-500">
-							Please confirm this is the code shown in your terminal
-						</p>
+						<h1 className="text-lg text-gray-100">Login cancelled</h1>
+						<p className="text-sm text-gray-500">You can return to your CLI.</p>
 					</div>
 				</div>
-				<div>
-					<div className="grid grid-flow-col gap-1 pt-6 leading-none lg:gap-3 auto-cols-auto">
-						{code?.split("").map((char, i) => (
-							<CodeCharacter char={char} key={`${char}-${i}`} />
-						))}
+			) : (
+				<div className="flex flex-col">
+					<div className="flex ">
+						<div className="flex justify-center items-center pr-4">
+							<Fingerprint className="text-gray-100" />
+						</div>
+						<div className="flex-col">
+							<h1 className="text-lg text-gray-100">Device confirmation</h1>
+							<p className="text-sm text-gray-500">
+								Please confirm this is the code shown in your terminal
+							</p>
+						</div>
 					</div>
-					<div className="flex justify-center pt-6">
-						<div className="flex items-center">
-							<Button
-								variant="default"
-								className="mr-2"
-								onClick={() => verify(opts)}
-								disabled={loading}
-							>
-								Confirm code
-							</Button>
-							<Button variant="outline" onClick={() => cancel(_redirect)}>
-								Cancel
-							</Button>
+					<div>
+						<div className="grid grid-flow-col gap-1 pt-6 leading-none lg:gap-3 auto-cols-auto">
+							{code?.split("").map((char, i) => (
+								<CodeCharacter char={char} key={`${char}-${i}`} />
+							))}
+						</div>
+						<div className="flex justify-center pt-6">
+							<div className="flex items-center">
+								<Button
+									variant="default"
+									className="mr-2"
+									onClick={() => verify(opts)}
+									disabled={loading}
+								>
+									Confirm code
+								</Button>
+								<Button variant="outline" onClick={() => cancel(_redirect)}>
+									Cancel
+								</Button>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			)}
 			<Toaster />
 		</div>
 	);
